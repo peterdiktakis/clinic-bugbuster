ec123da1e59180ff6ea316ba9a74fcdda68736d7
Peter Diktakis <peterdiktakis@gmail.com>
2014-12-03 12:52:25 -0500

    UI Update
    Working on Patient Registration

--- application/views/receptionregistration.php
+++ application/views/receptionregistration.php
@@ -3,12 +3,26 @@
 </div>
 
 <div class="well">
-
   <?php echo form_open('patientregistration', array('role' => 'form'));?>
   <?php echo validation_errors();?>
-    <div class="form-group">
-      <label for="exampleInputEmail1">Email address</label>
-      <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Enter email">
+  <div class="form-group">
+    <div class="row">
+      <label class = "col-sm-2 text-center" for="ramq">RAMQ:</label>
+      <div class="col-sm-6">
+        <input readonly = 'readonly' type="email" class="form-control" name="ramq" placeholder="RAMQ" value=<?php echo (isset($patient['RAMQ_ID']) ? $patient['RAMQ_ID'] : '') ?>>
+      </div>
     </div>
-  </form>
+  </div>
+  <div class="form-group">
+    <div class="row">
+      <label class = "col-sm-2 text-center" for="firstName">Patient Name:</label>
+      <div class="col-sm-2">
+        <input type="email" class="form-control" name="firstName" placeholder="First Name" value=<?php echo (isset($patient['FIRST_NAME']) ? $patient['FIRST_NAME'] : '') ?>>
+      </div>
+      <div class="col-sm-2">
+        <input type="email" class="form-control" name="lastName" placeholder="Last Name" value=<?php echo (isset($patient['LAST_NAME']) ? $patient['LAST_NAME'] : '') ?>>
+      </div>
+    </div>
+  </div>
+</form>
 </div>
